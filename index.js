9d4eb66033cef09e01278a9ab46d23386c568a20
fiolam <fiola.mulliqi@wework.com>
2018-08-06 23:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ return response.json();
 }
 
 function fetchHouses() {
-  
-}
-
-
+  // use fetch to make a request to the this api /house instead of /books
+  // with the JSON that comes back from that request, put the house name and region and whatever else on the page
+fetch("https://anapioficeandfire.com/api/houses")
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
