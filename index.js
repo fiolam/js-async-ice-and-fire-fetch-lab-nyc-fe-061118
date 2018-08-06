fec3699cb34eb7c5257154499e9fbc5650acb8fc
fiolam <fiola.mulliqi@wework.com>
2018-08-06 23:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ fetch("https://anapioficeandfire.com/api/houses")
 })
 .then(renderHouses)
 }
+function renderHouses(json) {
+  const main = document.querySelector('main')
+  json.forEach(house => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${house.name}</h2>`
+    main.appendChild(h2)
+  })
+}
 
 
 
