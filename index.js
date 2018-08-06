f010675f9a5c1349857c9997ed4e583de4a36dad
fiolam <fiola.mulliqi@wework.com>
2018-08-06 23:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ fetch("https://anapioficeandfire.com/api/books")
 .then(function(response){
 return response.json();
 })
-.then(function(json){
-return response.renderBooks()
-})
+.then(renderBooks)
+
 }
 
 
