4573a59dc817baa77b55352da28cc35dace3313f
fiolam <fiola.mulliqi@wework.com>
2018-08-06 23:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+
+fetch("https://anapioficeandfire.com/api/books")
+.then()
 }
-fetch()""
+
+
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
